161477ef2316b18b40a7499742818c18d2001b52
Totom3 <suptom3@gmail.com>
2018-08-29 17:00:23 -0400

    Better organize events & code in Tictactoe GUI

--- tictactoe-gui/src/com/devtomermoran/tictactoe/Main.java
+++ tictactoe-gui/src/com/devtomermoran/tictactoe/Main.java
@@ -1,19 +1,20 @@
 package com.devtomermoran.tictactoe;
 
-import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.function.Consumer;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
@@ -21,13 +22,12 @@ public class Main extends JFrame {
 
 	private JPanel contentPane;
 
-	private PreGameHeader preGameHeader;
-	private GameHeader gameHeader;
 	private GameBoard gameBoard;
+	private PreGameHeader preGameHeader;
 	private PostGameFooter postGameFooter;
 
 	private Main() {
-		setSize(720, 640);
+		setSize(740, 680);
 		setTitle("TicTacToe Game");
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -38,60 +38,52 @@ public class Main extends JFrame {
 		this.contentPane = new JPanel(new GridBagLayout());
 
 		this.preGameHeader = new PreGameHeader();
-		this.gameHeader = new GameHeader();
 		this.postGameFooter = new PostGameFooter();
-		this.gameBoard = new GameBoard(endGameListener());
+		this.gameBoard = new GameBoard();
 
 		// Construct GUI
-		gameHeader.initGUI();
-		preGameHeader.initGUI((ActionEvent ae) -> {
-			gameHeader.updateNames(preGameHeader.getPlayer1(), preGameHeader.getPlayer2());
+		gameBoard.initGUI();
+		preGameHeader.initGUI();
 
-			preGameHeader.setVisible(false);
-			gameHeader.setVisible(true);
-
-			gameBoard.startGame();
-		});
-
-		gameHeader.setVisible(false);
 		postGameFooter.setVisible(false);
 
 		Insets zero = new Insets(0, 0, 0, 0);
 		contentPane.add(preGameHeader, constraints(0, 0, 1, zero));
-		contentPane.add(gameHeader, constraints(0, 0, 1, zero));
 		contentPane.add(gameBoard, constraints(0, 1, 1, zero));
 		contentPane.add(postGameFooter, constraints(0, 2, 1, zero));
 		this.setContentPane(contentPane);
 	}
 
-	private Consumer<Integer> endGameListener() {
-		return (Integer winner) -> {
-			System.out.println("End of game! " + winner);
-
-			String winnerName;
-			switch (winner) {
-				case -1:
-					winnerName = null;
-					break;
-				case 1:
-					winnerName = preGameHeader.getPlayer1();
-					break;
-				case 2:
-					winnerName = preGameHeader.getPlayer2();
-					break;
-				default:
-					throw new AssertionError();
-			}
-
-			postGameFooter.onWin(winnerName);
-
-			preGameHeader.setVisible(true);
-			gameHeader.setVisible(false);
-			postGameFooter.setVisible(true);
-		};
+	private void startGame() {
+		preGameHeader.setVisible(false);
+		gameBoard.startGame(preGameHeader.getPlayer1(), preGameHeader.getPlayer2());
 	}
 
-	private static class PreGameHeader extends JPanel {
+	private void endGame(int winner) {
+		System.out.println("End of game! " + winner);
+
+		String winnerName;
+		switch (winner) {
+			case -1:
+				winnerName = null;
+				break;
+			case 1:
+				winnerName = preGameHeader.getPlayer1();
+				break;
+			case 2:
+				winnerName = preGameHeader.getPlayer2();
+				break;
+			default:
+				throw new AssertionError();
+		}
+
+		postGameFooter.onWin(winnerName);
+
+		preGameHeader.setVisible(true);
+		postGameFooter.setVisible(true);
+	}
+
+	private class PreGameHeader extends JPanel {
 
 		private final JLabel label1, label2;
 		private final JTextField field1, field2;
@@ -107,13 +99,15 @@ public class Main extends JFrame {
 			this.startButton = new JButton("Start Game");
 		}
 
-		private void initGUI(ActionListener onStart) {
+		private void initGUI() {
+			// 1. Add child elements to this pane
 			this.add(label1, constraints(0, 0, 1, new Insets(15, 0, 0, 5)));
 			this.add(field1, constraints(1, 0, 1, new Insets(15, 0, 0, 20)));
 			this.add(label2, constraints(2, 0, 1, new Insets(15, 0, 0, 5)));
 			this.add(field2, constraints(3, 0, 0, new Insets(15, 0, 0, 0)));
 			this.add(startButton, constraints(0, 1, 4, new Insets(20, 0, 20, 0)));
 
+			// 2. Style child elements
 			Font boldFont = new Font("Monospace", Font.BOLD, 15);
 			Font plainFont = new Font("Monospace", Font.PLAIN, 15);
 			label1.setFont(boldFont);
@@ -122,8 +116,9 @@ public class Main extends JFrame {
 			field2.setFont(plainFont);
 			startButton.setFont(boldFont);
 
+			// 3. Set up event handlers
 			startButton.setEnabled(false);
-			startButton.addActionListener(onStart);
+			startButton.addActionListener(e -> startGame());
 
 			DocumentListener listener = new DocumentListener() {
 				@Override
@@ -159,48 +154,42 @@ public class Main extends JFrame {
 		}
 	}
 
-	private static class GameHeader extends JPanel {
+	private static final char SYMBOLS[] = {'X', 'O'};
+	private static final Font NAME_FONTS[] = {
+		new Font("monospace", Font.BOLD, 35),
+		new Font("monospace", Font.PLAIN, 30)
+	};
 
-		private final JLabel label1, label2;
+	private class GameBoard extends JPanel {
 
-		private GameHeader() {
-			super(new GridBagLayout());
-
-			this.label1 = new JLabel();
-			this.label2 = new JLabel();
-			this.label2.setAlignmentX(RIGHT_ALIGNMENT);
-
-			Font font = new Font("monospace", Font.BOLD, 30);
-			this.label1.setFont(font);
-			this.label2.setFont(font);
-		}
-
-		private void initGUI() {
-			this.add(label1, constraints(0, 0, 1, new Insets(0, 0, 20, 150)));
-			this.add(label2, constraints(1, 0, 1, new Insets(0, 150, 20, 0)));
-		}
-
-		private void updateNames(String name1, String name2) {
-			label1.setText(name1);
-			label2.setText(name2);
-		}
-	}
-
-	private static class GameBoard extends JPanel {
-
-		private static final char SYMBOLS[] = {'X', 'O'};
+		private final JLabel playerName1, playerName2;
 
 		private final JButton buttons[][];
 		private final char board[][] = new char[3][3];
 
 		private int moveCounter;
-		private final Consumer<Integer> endGameListener;
 
-		private GameBoard(Consumer<Integer> endgameListener) {
+		private GameBoard() {
 			super(new GridBagLayout());
-			this.endGameListener = endgameListener;
+
+			this.playerName1 = new JLabel();
+			this.playerName2 = new JLabel();
 
 			this.buttons = new JButton[3][3];
+
+		}
+
+		private void initGUI() {
+			// 1. Initialize player names
+			playerName1.setFont(NAME_FONTS[0]);
+			playerName2.setFont(NAME_FONTS[1]);
+			playerName2.setHorizontalAlignment(SwingConstants.LEFT);
+			playerName2.setHorizontalAlignment(SwingConstants.RIGHT);
+
+			this.add(playerName1, constraints(0, 0, 1, new Insets(0, 0, 0, 0)));
+			this.add(playerName2, constraints(1, 0, 2, new Insets(0, 0, 0, 0)));
+
+			// 2. Initialize the buttons grid
 			for (int i = 0; i < 3; ++i) {
 				for (int j = 0; j < 3; ++j) {
 					JButton button = new JButton("X");
@@ -210,7 +199,7 @@ public class Main extends JFrame {
 
 					GridBagConstraints constr = new GridBagConstraints();
 					constr.gridx = j;
-					constr.gridy = i;
+					constr.gridy = i + 1;
 					this.add(button, constr);
 
 					button.addActionListener(clickListener(button, i, j));
@@ -218,7 +207,6 @@ public class Main extends JFrame {
 					this.buttons[i][j] = button;
 				}
 			}
-
 		}
 
 		private ActionListener clickListener(JButton button, int i, int j) {
@@ -226,21 +214,32 @@ public class Main extends JFrame {
 				if (board[i][j] != 0) {
 					return;
 				}
-
+				
 				// Update character
 				int player = moveCounter % 2;
 				board[i][j] = SYMBOLS[player];
 				button.setText(String.valueOf(SYMBOLS[player]));
 
+				playerName1.setFont(NAME_FONTS[player ^ 1]);
+				playerName2.setFont(NAME_FONTS[player]);
+
+				// Increment move counter to easily detect ties
 				++moveCounter;
+
+				// Check for end of game
 				int winner = checkWinner(player);
 				if (winner != 0) {
-					endGameListener.accept(winner);
+					endGame(winner);
+					playerName1.setVisible(false);
+					playerName2.setVisible(false);
 				}
 			};
 		}
 
-		private void startGame() {
+		private void startGame(String name1, String name2) {
+			playerName1.setText(name1 + " (" + SYMBOLS[0] + ")");
+			playerName2.setText(name2 + " (" + SYMBOLS[1] + ")");
+
 			for (int i = 0; i < 3; ++i) {
 				for (int j = 0; j < 3; ++j) {
 					board[i][j] = 0;
@@ -288,7 +287,7 @@ public class Main extends JFrame {
 		}
 	}
 
-	private static class PostGameFooter extends JPanel {
+	private class PostGameFooter extends JPanel {
 
 		private final JLabel label;
 
@@ -323,6 +322,7 @@ public class Main extends JFrame {
 		constr.gridwidth = width;
 		constr.gridheight = 1;
 		constr.insets = insets;
+		constr.fill = GridBagConstraints.BOTH;
 		return constr;
 	}
 }
